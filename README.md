66ed7da71ddef42b9d1e8fa049e35e0146ec090d
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-07-19 00:27:35 +0100

    plugin validation function

--- README.md
+++ README.md
@@ -6,8 +6,14 @@ A collection of plugins for use in the [Endovelicus](https://github.com/SrGesus/
   - [ ] Building
   - [ ] Uploading Releases
 - [ ] Currency rate fetching plugin
+  - [ ] Make use of a api to get currency rates and names like [frankfurter](https://www.frankfurter.app/) (unfortunately no symbols) or [freecurrencyapi](https://freecurrencyapi.com/docs/latest#request-parameters) (symbols but needs api key)
+  - [ ] Endpoint for refreshing currencies
 
 ## Plugins
+
+Plugin implementations should include:
+- validate() -> bool
+Returning whether the plugin configuration is valid
 
 ### Currency Api
 Plugin that fetches data about currencies from an api and adds it to Endovelicus.
--- currencyapi/src/lib.rs
+++ currencyapi/src/lib.rs
@@ -1,6 +1,26 @@
+use std::collections::BTreeMap;
+
 use extism_pdk::*;
 
+fn get_url() -> Result<String, Error> {
+    Ok(config::get("url")?
+    .unwrap_or("https://api.freecurrencyapi.com/v1".to_string()))
+}
+
+fn get_api_key() -> Result<String, Error> {
+    config::get("api_key")?
+    .ok_or(Error::msg("Api key not found in configuration."))
+}
+
 #[plugin_fn]
-pub fn get() -> FnResult<String>{
-    Ok("Hello, world!".to_owned())
+pub fn test() -> FnResult<()> {
+    let url = get_url()?;
+
+    let api_key = get_api_key()?;
+
+    let status_url = format!("{url}/status?apikey={api_key}");
+    match http::request::<()>(&HttpRequest { url: status_url, headers: BTreeMap::new(), method: None }, None)?.status_code() {
+        200..=299 => Ok(()),
+        0..=199 | 300.. => Err(Error::msg("Api key not found in configuration.").into()),
+    }
 }
